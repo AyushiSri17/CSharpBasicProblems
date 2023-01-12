9fb9d6ac49781b440a218aea33588040e422795b
“AyushiSri17” <srivastavaayushi7110@gmail.com>
2023-01-12 21:04:16 +0530

    [Ayushi] Adding Calculator Functionality

---
+++ CSharpBasicProblems/Calculator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpBasicProblems
+{
+    internal class Calculator
+    {
+        public int first, second, sum, sub, mult, div;
+
+        public Calculator(int firstNumber, int secondNumber)
+        { 
+            first = firstNumber;
+            second = secondNumber;
+        }
+
+        public void Addition()
+        { 
+            sum = first + second;
+            Console.WriteLine("Addition of two number is: "+sum);
+        }
+        public void Substraction()
+        {
+            sub = first - second;
+            Console.WriteLine("Substraction of two number is: " + sub);
+        }
+        public void Multiplication()
+        {
+            mult = first * second;
+            Console.WriteLine("Multiplication of two number is: " + mult);
+        }
+        public void Division()
+        {
+            div = first / second;
+            Console.WriteLine("Division of two number is: " + div);
+        }
+    }
+}
--- CSharpBasicProblems/Program.cs
+++ CSharpBasicProblems/Program.cs
@@ -11,6 +11,17 @@ namespace CSharpBasicProblems
         static void Main(string[] args)
         {
             Console.WriteLine("WELCOME TO C# PROGRAM");
+
+            Console.WriteLine("Please enter first number");
+            int first=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter second number");
+            int second=Convert.ToInt32(Console.ReadLine());
+
+            Calculator calculator= new Calculator(first, second);
+            calculator.Addition();
+            calculator.Substraction();
+            calculator.Multiplication();
+            calculator.Division();
             Console.ReadLine();
         }
     }
