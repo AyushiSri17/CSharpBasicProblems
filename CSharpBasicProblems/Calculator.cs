1e7aed9f8a9bcbb160d5d498b616181be17c18cc
“AyushiSri17” <srivastavaayushi7110@gmail.com>
2023-01-14 08:20:08 +0530

    [Ayushi] Adding Calculator Functionality

--- CSharpBasicProblems/Calculator.cs
+++ CSharpBasicProblems/Calculator.cs
@@ -6,34 +6,42 @@ using System.Threading.Tasks;
 
 namespace CSharpBasicProblems
 {
+    //Defining class naming calculator
     internal class Calculator
     {
-        public int first, second, sum, sub, mult, div;
+        //Instance Variables
+        public int first, second, sum, difference, mult;
+        public float div;
 
+        //Constructor for initialization
         public Calculator(int firstNumber, int secondNumber)
         { 
             first = firstNumber;
             second = secondNumber;
         }
 
+        //Additon method declaration
         public void Addition()
         { 
             sum = first + second;
             Console.WriteLine("Addition of two number is: "+sum);
         }
+        //Substraction method declaration
         public void Substraction()
         {
-            sub = first - second;
-            Console.WriteLine("Substraction of two number is: " + sub);
+            difference = Math.Abs(first - second);
+            Console.WriteLine("Substraction of two number is: " + difference);
         }
+        //Multiplication method declaration
         public void Multiplication()
         {
             mult = first * second;
             Console.WriteLine("Multiplication of two number is: " + mult);
         }
+        //Division method declaration
         public void Division()
         {
-            div = first / second;
+           div = (float)first / second;
             Console.WriteLine("Division of two number is: " + div);
         }
     }
